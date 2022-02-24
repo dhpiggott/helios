bcd98eb1144e5ab5ccca158d9d34ac4d2a285b70
David Piggott <david@piggott.me.uk>
2022-02-24 21:53:44 +0000

    Add TODO

--- helios/src/main/scala/helios/Helios.scala
+++ helios/src/main/scala/helios/Helios.scala
@@ -414,6 +414,7 @@ object Helios extends App with Http4sClientDsl[Task]:
   def printState(lights: Map[String, Light]): RIO[Console, Unit] =
     RIO.foreach(lights.values)(light => putStrLn(light.toJson)).unit
 
+  // TODO: Use https://developers.meethue.com/develop/application-design-guidance/hue-bridge-discovery/
   val bridgeApiBaseUriLayer = env("BRIDGE_IP")
     .flatMap(IO.fromOption(_))
     .orElseFail("BRIDGE_IP must be set.")
